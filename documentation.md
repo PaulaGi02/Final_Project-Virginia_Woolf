bbf655eb7af935455b980f8bbf2de85ee2c7b336
PaulaGi02 <paula.giensch@stud.leuphana.de>
2026-01-11 12:04:19 +0800

    markov

--- documentation.md
+++ documentation.md
@@ -13,3 +13,8 @@ also clarissa taking in what other people thought about her. but difficult becau
 ### 10.1.
 - virtual enviroment: set up a clean environment so installs don’t conflict.
 - created main.py to load both text files and just test with a simple len() if importation worked 
+- cleaning text from quotes and whitespaces etc.
+- Clarissa sample: That he is probably the roses absolutely lovely; first getting sent down from the Queen, thought Clarissa, too, gave her hands already, quite enough of her time she looked out again.
+Septimus sample: It spared him, the drowned sailor; the immortal ode; the heat wave of the sky, muttering, clasping his rock, like a Skye terrier snuffed his brain made her hold his eyes as he could not cut down trees
+- First try with markov chain and tries = 10 and state_size = 1
+- even though it is gibberigh clear which character is who
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from pathlib import Path
+from scr.processes import clean_text
+import markovify
+import spacy
+from collections import Counter
+import numpy as np
+
+nlp = spacy.load("en_core_web_md")
+
+def load_text(path: str) -> str:
+    return Path(path).read_text()
 
 with open ("data/clarissa.txt") as f:
     clarissa_txt = f.read()
@@ -6,4 +16,13 @@ with open ("data/clarissa.txt") as f:
 with open ("data/septimus.txt") as f:
     septimus_txt = f.read()
 
-print (len(septimus_txt), len(clarissa_txt))
+clarissa_text = clean_text(clarissa_txt)
+septimus_text = clean_text(septimus_txt)
+
+clarissa_model = markovify.Text(clarissa_text, state_size=1)
+septimus_model = markovify.Text(septimus_text, state_size=1)
+
+print("Clarissa sample:", clarissa_model.make_sentence(tries=10))
+print("Septimus sample:", septimus_model.make_sentence(tries=10))
+
+print(len(clarissa_text), len(septimus_text))
